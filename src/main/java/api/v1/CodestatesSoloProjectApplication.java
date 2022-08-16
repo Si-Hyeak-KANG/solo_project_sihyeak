b9c4b1706d692b9183a86d1679c3b709fa95f3f7
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-16 14:20:53 +0900

    first commit

---
+++ src/main/java/api/v1/CodestatesSoloProjectApplication.java
@@ -0,0 +1,13 @@
+package api.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestatesSoloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodestatesSoloProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/v1/CodestatesSoloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package api.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestatesSoloProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
